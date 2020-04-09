82b30ce0aa107729a088539af825cbff11a1b8af
harshp45 <harshpatel235027@gmail.com>
2020-04-09 02:55:31 -0400

    Updated

--- routes/api/currency.js
+++ routes/api/currency.js
@@ -5,95 +5,45 @@ const auth = require('../../middleware/auth');
 const rateslist = require('../../models/Rates');
 const router = express.Router();
 
-// var basecurrency = "CAD";
-// var apiURL = "https://api.exchangeratesapi.io/latest?base="+basecurrency+"&HTTP/1.1";
+var basecurrency = "CAD";
+var apiURL = "https://api.exchangeratesapi.io/latest?base="+basecurrency+"&HTTP/1.1";
 
-router.post('/base', (req,res) => 
+
+
+//fetching live currency rates through API
+http.get(apiURL, function (res) 
 {
-    var basecurrency = req.body.base;
-    var apiURL = "https://api.exchangeratesapi.io/latest?base="+basecurrency+"&HTTP/1.1";
+    var body = "";
+    res.on('data', function (data) {
+        body += data;
+        //console.log("Data: "+body);
+    });
 
-    res.send("Base Currency Set");
-    console.log(apiURL)
-
-    //fetching live currency rates through API
-    http.get(apiURL, function (res) 
+    //Fetching Rates
+    router.get('/list', auth, async (req, res) => 
     {
-        var body = "";
-        res.on('data', function (data) {
-            body += data;
-            //console.log("Data: "+body);
-        });
+        try {
+            const RateDb = await rateslist.findOne();
+            //const inr = RateDb.rates[0].INR;
+            //console.log(JSON.stringify(inr));
+            res.send(RateDb);
+        }
+        catch (err) {
+            res.status(500).send('Server Error');
+        }
+    });
 
-        //Fetching Rates
-        router.get('/list', auth, async (req, res) => 
+    //Adding the currency rates into MongoDB just for reference
+    router.post('/add', auth, async (req, res) => 
+    {
+        try 
         {
-            try {
-                const RateDb = await rateslist.findOne();
-                //const inr = RateDb.rates[0].INR;
-                //console.log(JSON.stringify(inr));
-                res.send(RateDb);
-            }
-            catch (err) {
-                res.status(500).send('Server Error');
-            }
-        });
+            var rateResponse = JSON.parse(body);
+            const newrate = new rateslist({
 
-        //Adding the currency rates into MongoDB just for reference
-        router.post('/add', auth, async (req, res) => 
-        {
-            try 
-            {
-                var rateResponse = JSON.parse(body);
-                const newrate = new rateslist({
-
-                    base: rateResponse.base,
-                    date: rateResponse.date,
-                    rates:
-                        [{
-                            EUR: rateResponse.rates.EUR,
-                            CAD: rateResponse.rates.CAD,
-                            HKD: rateResponse.rates.HKD,
-                            ISK: rateResponse.rates.ISK,
-                            PHP: rateResponse.rates.PHP,
-                            DKK: rateResponse.rates.DKK,
-                            HUF: rateResponse.rates.HUF,
-                            CZK: rateResponse.rates.CZK,
-                            GBP: rateResponse.rates.GBP,
-                            RON: rateResponse.rates.RON,
-                            SEK: rateResponse.rates.SEK,
-                            IDR: rateResponse.rates.IDR,
-                            INR: rateResponse.rates.INR,
-                            BRL: rateResponse.rates.BRL,
-                            USD: rateResponse.rates.USD,
-                            MXN: rateResponse.rates.MXN,
-                            SGD: rateResponse.rates.SGD,
-                            AUD: rateResponse.rates.AUD,
-                            ILS: rateResponse.rates.ILS,
-                            KRW: rateResponse.rates.KRW,
-                            PLN: rateResponse.rates.PLN
-                        }]
-                });
-
-                const nRate = await newrate.save();
-                res.json(nRate);
-                res.end();
-            }
-            catch (err) {
-                res.status(500).send('Server Error');
-            }
-        });
-
-        //Updating the currency rates into MongoDB to get updated rates on React App
-        router.put('/update', auth, async (req, res) => 
-        {
-            try {
-                var rateResponse = JSON.parse(body);
-                const newrate = await rateslist.findById("5e8b61c568a5df3a8414cbdc");
-
-                newrate.base = rateResponse.base,
-                    newrate.date = rateResponse.date,
-                    newrate.rates =
+                base: rateResponse.base,
+                date: rateResponse.date,
+                rates:
                     [{
                         EUR: rateResponse.rates.EUR,
                         CAD: rateResponse.rates.CAD,
@@ -117,15 +67,59 @@ router.post('/base', (req,res) =>
                         KRW: rateResponse.rates.KRW,
                         PLN: rateResponse.rates.PLN
                     }]
+            });
 
-                const nRate = await newrate.save();
-                res.json(nRate);
-                res.end();
-            }
-            catch (err) {
-                res.status(500).send('Server Error');
-            }
-        });
+            const nRate = await newrate.save();
+            res.json(nRate);
+            res.end();
+        }
+        catch (err) {
+            res.status(500).send('Server Error');
+        }
+    });
+
+    //Updating the currency rates into MongoDB to get updated rates on React App
+    router.put('/update', auth, async (req, res) => 
+    {
+        try {
+            var rateResponse = JSON.parse(body);
+            const newrate = await rateslist.findById("5e8b61c568a5df3a8414cbdc");
+
+            newrate.base = rateResponse.base,
+                newrate.date = rateResponse.date,
+                newrate.rates =
+                [{
+                    EUR: rateResponse.rates.EUR,
+                    CAD: rateResponse.rates.CAD,
+                    HKD: rateResponse.rates.HKD,
+                    ISK: rateResponse.rates.ISK,
+                    PHP: rateResponse.rates.PHP,
+                    DKK: rateResponse.rates.DKK,
+                    HUF: rateResponse.rates.HUF,
+                    CZK: rateResponse.rates.CZK,
+                    GBP: rateResponse.rates.GBP,
+                    RON: rateResponse.rates.RON,
+                    SEK: rateResponse.rates.SEK,
+                    IDR: rateResponse.rates.IDR,
+                    INR: rateResponse.rates.INR,
+                    BRL: rateResponse.rates.BRL,
+                    USD: rateResponse.rates.USD,
+                    MXN: rateResponse.rates.MXN,
+                    SGD: rateResponse.rates.SGD,
+                    AUD: rateResponse.rates.AUD,
+                    ILS: rateResponse.rates.ILS,
+                    KRW: rateResponse.rates.KRW,
+                    PLN: rateResponse.rates.PLN
+                }]
+
+            const nRate = await newrate.save();
+            res.json(nRate);
+            res.end();
+        }
+        catch (err) {
+            res.status(500).send('Server Error');
+        }
     });
 });
+
 module.exports = router;
