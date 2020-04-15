2c32374715dcecb2cd959a0689b7c758f9dec656
harshp45 <harshpatel235027@gmail.com>
2020-04-14 22:40:46 -0400

    Updated

--- login/routes/user.js
+++ login/routes/user.js
@@ -30,7 +30,7 @@ router.post('/', (req, res) => {
 })
 
 router.post(
-    '/login',
+    '/logins',
     function (req, res, next) {
         console.log('routes/user.js, login, req.body: ');
         console.log(req.body)
